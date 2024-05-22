804cfc2cf1ac82461e3a3078bbac9e4d2039550b
Dmitri Zavadski <dzdimati@gmail.com>
2024-05-22 17:50:42 +0300

    fix dict

--- task03/tests/test_hello_world/test_success.py
+++ task03/tests/test_hello_world/test_success.py
@@ -5,5 +5,11 @@ class TestSuccess(HelloWorldLambdaTestCase):
 
     def test_success(self):
         # self.assertEqual(self.HANDLER.handle_request(dict(), dict()), "Hello from Lambda")
+        # self.assertEqual(self.HANDLER.handle_request(dict(), dict()),
+        #                  "{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}")
+
         self.assertEqual(self.HANDLER.handle_request(dict(), dict()),
-                         "{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}")
+                         {
+                             "statusCode": 200,
+                             "message": "Hello from Lambda"
+                         })
