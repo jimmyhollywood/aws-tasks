e38d3a4ece29b01a08ba889ac88c852178cc1cd4
Dmitri Zavadski <dzdimati@gmail.com>
2024-06-27 19:24:57 +0300

    add 8 task

---
+++ task08/src/lambdas/api_handler/handler.py
@@ -0,0 +1,27 @@
+from commons.log_helper import get_logger
+from commons.abstract_lambda import AbstractLambda
+import requests
+import json
+
+_LOG = get_logger('ApiHandler-handler')
+
+
+class ApiHandler(AbstractLambda):
+
+    def validate_request(self, event) -> dict:
+        pass
+
+    def handle_request(self, event, context):
+        """
+        Explain incoming event here
+        """
+        response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m")
+        # todo implement business logic
+        return json.dumps(response)
+
+
+HANDLER = ApiHandler()
+
+
+def lambda_handler(event, context):
+    return HANDLER.lambda_handler(event=event, context=context)
