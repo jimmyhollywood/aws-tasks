4e2bc2b93400f86e340a47cd5919ad2ad07745ec
Dmitri Zavadski <dzdimati@gmail.com>
2024-06-28 13:05:26 +0300

    some fixes

--- task08/src/lambdas/api_handler/handler.py
+++ task08/src/lambdas/api_handler/handler.py
@@ -15,9 +15,10 @@ class ApiHandler(AbstractLambda):
         """
         Explain incoming event here
         """
+        print(event)
         response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m")
         # todo implement business logic
-        return json.dumps(response)
+        return response.json()
 
 
 HANDLER = ApiHandler()
