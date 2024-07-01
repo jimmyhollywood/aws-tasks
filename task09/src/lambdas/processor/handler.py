1e673fd9f34649a8c0d78b4e59117b1d99f011b0
Dmitri Zavadski <dzdimati@gmail.com>
2024-07-01 11:25:26 +0300

    fix table name

--- task09/src/lambdas/processor/handler.py
+++ task09/src/lambdas/processor/handler.py
@@ -17,7 +17,7 @@ class Processor(AbstractLambda):
         """
         Explain incoming event here
         """
-        table_name = os.environ['TARGET_TABLE']
+        table_name = "cmtr-27efb7c4-Weather-test"
         db = boto3.resource("dynamodb")
         table = db.Table(table_name)
         response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m")
